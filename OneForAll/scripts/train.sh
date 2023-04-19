df7a2b8918f645f2ccce747a76dd2968c188342f
DongLin <1250626937@qq.com>
2023-04-19 23:46:00 +0800

    MOD: train script

--- OneForAll/scripts/train.sh
+++ OneForAll/scripts/train.sh
@@ -1,4 +1,4 @@
-export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
+export CUDA_VISIBLE_DEVICES=0,1
 
 # kill -9 $(lsof -t /dev/nvidia*)
 # sleep 1s
@@ -8,6 +8,7 @@ export CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7
 config=configs/vitbase_retrieval.py
 
 #python3 -m paddle.distributed.launch --log_dir=./logs/vitbase_retrieval --gpus="0,1,2,3,4,5,6,7"  tools/ufo_train.py --config-file ${config} #--resume 
-/root/paddlejob/workspace/env_run/anaconda3/envs/py37_meta_pd-2.3.0_cu11/bin/python3.7 \
--m paddle.distributed.launch --log_dir=./logs/vitbase_retrieval --gpus="0,1,2,3,4,5,6,7"  tools/ufo_train.py --config-file ${config} #--resume
+# /root/paddlejob/workspace/env_run/anaconda3/envs/py37_meta_pd-2.3.0_cu11/bin/python3.7 \
+# -m paddle.distributed.launch --log_dir=./logs/vitbase_retrieval --gpus="0,1,2,3,4,5,6,7"  tools/ufo_train.py --config-file ${config} #--resume
 
+python -m paddle.distributed.launch --log_dir=logs/train --gpus="0,1"  tools/ufo_train.py --config-file ${config} #--resume
