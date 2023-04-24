2a9128696f5bde0e77e1d441dc5f98cab0bc130e
DongLin <1250626937@qq.com>
2023-04-24 15:59:46 +0800

    MOD: aug param

--- OneForAll/data/data_aug.py
+++ OneForAll/data/data_aug.py
@@ -53,12 +53,11 @@ def aug_vehicle_image():
     clean_path = 'OneForAll/data/datasets/vehicle/train/train_images_clean'
     aug_path = 'OneForAll/data/datasets/vehicle/train/train_images'
     p = ready_for_aug(raw_path, clean_path)
-    p.skew_tilt(probability=0.6, magnitude=0.4)
-    p.rotate(probability=0.6, max_left_rotation=25, max_right_rotation=25)
-    p.flip_left_right(probability=0.6)
-    p.crop_centre(probability=0.6, percentage_area=0.7)
-    p.random_erasing(probability=0.6, rectangle_area=0.2)
-    p.sample(2 * len(os.listdir(clean_path)))
+    p.rotate(probability=0.7, max_left_rotation=10, max_right_rotation=10)
+    p.flip_left_right(probability=0.5)
+    p.flip_top_bottom(probability=0.5)
+    p.random_erasing(probability=0.3, rectangle_area=0.1)
+    p.sample(3 * len(os.listdir(clean_path)))
     move_aug_result(clean_path, aug_path, save_path)
 
 
@@ -68,12 +67,11 @@ def aug_pedestrian_image():
     clean_path = 'OneForAll/data/datasets/pedestrian/train/train_images_clean'
     aug_path = 'OneForAll/data/datasets/pedestrian/train/train_images'
     p = ready_for_aug(raw_path, clean_path)
-    p.skew_tilt(probability=0.6, magnitude=0.4)
-    p.rotate(probability=0.6, max_left_rotation=15, max_right_rotation=15)
-    p.flip_left_right(probability=0.6)
-    p.crop_centre(probability=0.6, percentage_area=0.8)
-    p.random_erasing(probability=0.6, rectangle_area=0.15)
-    p.sample(2 * len(os.listdir(clean_path)))
+    p.rotate(probability=0.7, max_left_rotation=5, max_right_rotation=5)
+    p.flip_left_right(probability=0.5)
+    p.flip_top_bottom(probability=0.5)
+    p.random_erasing(probability=0.3, rectangle_area=0.1)
+    p.sample(3 * len(os.listdir(clean_path)))
     move_aug_result(clean_path, aug_path, save_path)
 
 
@@ -201,25 +199,23 @@ def merge_aug_result(data_type, config):
     aug_text_path = f'OneForAll/data/datasets/{data_type}/train/aug_text.json'
     aug_image_dict = json.load(open(aug_image_path, 'r'))
     aug_text_dict = json.load(open(aug_text_path, 'r'))
-    if config['raw']:
-        num_image = config['num_image'] - 1
-        num_text = config['num_text'] - 1
-    else:
-        num_image = config['num_image']
-        num_text = config['num_text']
+    # raw
+    num_image = config['num_image'] - 1
+    num_text = config['num_text'] - 1
     with open(label_path, 'r') as f, open(save_path, 'w') as f_w:
         for line in tqdm(f):
             line = line.strip().split('$')
             selected_image = []
             selected_text = []
-            if config['raw']:
-                selected_image.append(aug_image_dict[line[0]][-1])
-                selected_text.append(aug_text_dict[line[2]][0])
+            # raw
+            selected_image.append(aug_image_dict[line[0]][-1])
+            selected_text.append(aug_text_dict[line[2]][0])
             selected_image += random.sample(aug_image_dict[line[0]][:-1], k=min(len(aug_image_dict[line[0]][:-1]), num_image))
             selected_text += random.sample(aug_text_dict[line[2]][1:], k=min(len(aug_text_dict[line[2]][1:]), num_text))
-            for aug_image in selected_image:
-                for aug_text in selected_text:
-                    f_w.write(aug_image + '$' + line[1] + '$' + aug_text + '\n')
+            for aug_text in selected_text:
+                f_w.write(selected_image[0] + '$' + line[1] + '$' + aug_text + '\n')
+            for aug_image in selected_image[1:]:
+                f_w.write(aug_image + '$' + line[1] + '$' + selected_text[0] + '\n')
 
 
 if __name__ == '__main__':
@@ -233,17 +229,15 @@ if __name__ == '__main__':
     config = {
         'vehicle': {
             'image': True,
-            'num_image': 2,
+            'num_image': 3,
             'text': True,
-            'num_text': 2,
-            'raw': True
+            'num_text': 2
         },
         'pedestrian': {
             'image': True,
-            'num_image': 2,
+            'num_image': 3,
             'text': True,
-            'num_text': 2,
-            'raw': True
+            'num_text': 2
         }
     }
     for data_type in ['vehicle', 'pedestrian']:
